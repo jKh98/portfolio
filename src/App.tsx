b803c6c35986c163893d180587a66f7635f90214
Jihad Al Khurfan <jalkhurfan@gmail.com>
2021-02-28 17:15:37 +0200

    added i18n
    added path aliases

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from "react";
+import { ConfigProvider } from "antd";
+import { useTranslation } from "react-i18next";
 import "./App.css";
 
-import { Landing } from "./components/pages/landing/landing";
+import { Landing } from "./components/pages/landing/landing.component";
 
 function App() {
+  const { i18n } = useTranslation();
+
   return (
-    <div className="App">
-      <Landing />
-    </div>
+    <ConfigProvider direction={i18n.dir()}>
+      <div className="App">
+        <Landing />
+      </div>
+    </ConfigProvider>
   );
 }
 
