56a78b66e4bdd2eb03419214935dbf1b00676282
Jihad Al Khurfan <jalkhurfan@gmail.com>
2021-02-28 17:31:04 +0200

    fixed i18n config

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { ConfigProvider } from "antd";
 import { useTranslation } from "react-i18next";
 import "antd/dist/antd.css";
@@ -9,11 +9,14 @@ import { Landing } from "./components/pages/landing/landing.component";
 function App() {
   const { i18n } = useTranslation();
 
+  /** This useEffect rerenders dir */
+  useEffect(() => {
+    // do something on language change
+  }, [i18n.language]);
+
   return (
-    <ConfigProvider direction={i18n.dir()}>
-      <div className="App">
-        <Landing />
-      </div>
+    <ConfigProvider direction={i18n?.dir()}>
+      <Landing />
     </ConfigProvider>
   );
 }
