c63707c4ebcd510f3d13c06edeb351965a8a9fdf
Jihad Al Khurfan <jalkhurfan@gmail.com>
2021-03-03 00:34:15 +0200

    feat: modified about page

--- src/components/sections/about/about.component.tsx
+++ src/components/sections/about/about.component.tsx
@@ -1,6 +1,7 @@
 import React from "react";
+import { RiMouseLine } from "react-icons/all";
 import { useTranslation } from "react-i18next";
-import { Col, Layout, Row, Typography, Image } from "antd";
+import { Col, Layout, Row, Typography } from "antd";
 import { useThemeSwitcher } from "react-css-theme-switcher";
 
 import styles from "./about.module.css";
@@ -8,14 +9,20 @@ import DayBg from "&assets/images/day.jpg";
 import NightBg from "&assets/images/night.jpg";
 import { sections } from "&config/meta";
 
+import Profile from "&assets/images/profile.jpg";
+
 const { Title } = Typography;
-const lightGradient = "linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.8))";
+const lightGradient = "linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.4))";
 const darkGradient =
   "linear-gradient(rgba(255, 255, 255, 0.01), rgba(255, 255, 255, 0.1))";
 
 export function About() {
   const { currentTheme, themes } = useThemeSwitcher();
   const { t } = useTranslation();
+
+  const handleScroll = () => {
+    window.scrollTo({ top: window.innerHeight, behavior: "smooth" });
+  };
 
   return (
     <Layout
@@ -28,10 +35,27 @@ export function About() {
       }}
     >
       <Row justify="space-around" align="middle" className={styles.full}>
-        <Image width={120} height={120} className={styles.profile} />
-        <Title>{t(sections[0].title)}</Title>
-        {/* <Paragraph>{t(sections[0].description)}</Paragraph> */}
+        <Col>
+          <Row justify="center">
+            <img
+              className={styles.profile}
+              src={Profile}
+              alt="profile_picture"
+            />
+          </Row>
+          <Row justify="center">
+            <Col xs={22} sm={22} md={16} lg={16} xl={16} xxl={12}>
+              <Title level={1} className={styles.text}>
+                <b> {t(sections[0].title)}</b>
+              </Title>
+              <Title level={2} className={styles.text}>
+                {t(sections[0].description)}
+              </Title>
+            </Col>
+          </Row>
+        </Col>
       </Row>
+      <RiMouseLine onClick={handleScroll} className={styles.icon} />
     </Layout>
   );
 }
