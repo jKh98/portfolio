6c96a088bd2b97b61857aecf2b8b25013bb709b6
Jihad Al Khurfan <jalkhurfan@gmail.com>
2021-05-23 21:25:10 +0300

    added timeline

--- src/components/sections/experience/experience.component.tsx
+++ src/components/sections/experience/experience.component.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { List, Typography, Button } from "antd";
+import { Typography, Button, Timeline, Layout, Row, Col } from "antd";
 import { Parallax } from "rc-scroll-anim";
 
 import { experience as data } from "&config/data.json";
@@ -13,45 +13,55 @@ export function Experience() {
 
   return (
     <Section title={t("EXPERIENCE_HEADING")}>
-      <List
-        size="large"
-        bordered
-        itemLayout="vertical"
-        dataSource={data}
-        renderItem={({ occupation, date, position, url, items }, index) => (
-          <Parallax
-            key={index}
-            animation={{ opacity: 1, playScale: [0.05] }}
-            style={{ opacity: 0 }}
-          >
-            <List.Item
-              extra={
-                <Text type="secondary">
-                  <b>{t(date)}</b>
-                </Text>
-              }
-            >
-              <List.Item.Meta
-                title={<Title level={4}>{t(position)}</Title>}
-                description={
-                  <Button type="primary" size="small" shape="round" href={url}>
-                    {t(occupation)}
-                  </Button>
+      <Row>
+        <Col xs={22} sm={22} md={18} lg={18} xl={18} xxl={16}>
+          <Timeline mode="left">
+            {data.map(({ occupation, date, position, url, items }, index) => (
+              <Timeline.Item
+                label={
+                  <Parallax
+                    animation={{ opacity: 1, playScale: [0.05] }}
+                    style={{ opacity: 0 }}
+                  >
+                    <div>
+                      <Text type="secondary">
+                        <b>{t(date)}</b>
+                      </Text>
+                    </div>
+                  </Parallax>
                 }
-              />
-              <List
-                size="small"
-                dataSource={items}
-                renderItem={(item) => (
-                  <List.Item>
-                    <Text> {t(item)}</Text>
-                  </List.Item>
-                )}
-              />
-            </List.Item>
-          </Parallax>
-        )}
-      />
+              >
+                <Parallax
+                  key={index}
+                  animation={{ opacity: 1, playScale: [0.05] }}
+                  style={{ opacity: 0 }}
+                >
+                  <Layout>
+                    <div>
+                      <Title level={4}>{t(position)}</Title>
+                      <Button
+                        block={false}
+                        type="default"
+                        size="small"
+                        shape="round"
+                        href={url}
+                      >
+                        {t(occupation)}
+                      </Button>
+                    </div>
+                    <br />
+
+                    {items.map((item) => (
+                      <Text>- {t(item)}</Text>
+                    ))}
+                  </Layout>
+                  <br />
+                </Parallax>
+              </Timeline.Item>
+            ))}
+          </Timeline>
+        </Col>
+      </Row>
     </Section>
   );
 }
