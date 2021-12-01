495c48660d4441f4d772c942f68ad3546045b4d6
Jihad Al Khurfan <jalkhurfan@gmail.com>
2021-12-01 10:49:43 +0200

    has router

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
 import React, { useEffect } from "react";
 import { ConfigProvider } from "antd";
 import { useTranslation } from "react-i18next";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import ReactGA from "react-ga";
 import "antd/dist/antd.css";
 import "./App.css";
 
 import { Landing } from "&components/pages/landing/landing.component";
-import { gaKey } from "&config/meta";
+import { gaKey, sections } from "&config/meta";
 
 ReactGA.initialize(gaKey);
 
 function App() {
   const { i18n } = useTranslation();
+
+  useEffect(() => {
+    const validHashes = [...Object.values(sections), "home"];
+    console.log(window.location.hash);
+    if (window.location.hash.includes("#/")) {
+      window.location.hash = "";
+    }
+  }, []);
 
   /** This useEffect rerenders dir */
   useEffect(() => {
@@ -21,11 +29,11 @@ function App() {
 
   return (
     <ConfigProvider direction={i18n?.dir()}>
-      <BrowserRouter>
+      <HashRouter>
         <Switch>
           <Route exact path="/" component={Landing} />
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
     </ConfigProvider>
   );
 }
