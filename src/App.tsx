d9932222dcea3d97811039c98f60cd8c04f574c6
Jihad Al Khurfan <jalkhurfan@gmail.com>
2021-12-01 07:48:38 +0200

    gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { ConfigProvider } from "antd";
 import { useTranslation } from "react-i18next";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import ReactGA from "react-ga";
 import "antd/dist/antd.css";
 import "./App.css";
 
@@ -9,6 +10,11 @@ import { Landing } from "./components/pages/landing/landing.component";
 
 function App() {
   const { i18n } = useTranslation();
+
+  useEffect(() => {
+    ReactGA.initialize("UA-161722008-02");
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
 
   /** This useEffect rerenders dir */
   useEffect(() => {
