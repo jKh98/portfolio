143d9de4cc2b7390e7cef83275f09899ef50335a
Jihad Al Khurfan <jalkhurfan@gmail.com>
2022-01-07 01:27:15 +0200

    returned hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { ConfigProvider } from "antd";
 import { useTranslation } from "react-i18next";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import ReactGA from "react-ga";
 import "antd/dist/antd.css";
 import "./App.css";
@@ -35,11 +35,11 @@ function App() {
 
   return (
     <ConfigProvider direction={i18n?.dir()}>
-      <BrowserRouter>
+      <HashRouter>
         <Switch>
-          <Route exact path="/" component={Landing} />
+          <Route path="/" component={Landing} />
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
     </ConfigProvider>
   );
 }
